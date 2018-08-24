623bb98ae86745eeda4460033fc20e1a5c2a04b5
sbx_user1060 <sbx_user1060@ip-10-129-23-199.eu-west-2.compute.internal>
2018-08-24 10:36:08 +0000

    Fri Aug 24 10:36:08 UTC 2018

--- src/test/java/befaster/solutions/FIZ/FizzBuzzSolutionTest.java
+++ src/test/java/befaster/solutions/FIZ/FizzBuzzSolutionTest.java
@@ -10,16 +10,31 @@ import static org.junit.Assert.*;
 
 public class FizzBuzzSolutionTest {
 
-    private SumSolution sum;
+    private FizzBuzzSolution solution;
 
     @Before
     public void setUp() {
 
-        sum = new SumSolution();
+        solution = new FizzBuzzSolution();
     }
 
     @Test
-    public void compute_sum() {
-        assertThat(sum.compute(1, 1), equalTo(2));
+    public void fizzTest() {
+        assertThat(solution.fizzBuzz(3), equalTo("fizz"));
+    }
+
+    @Test
+    public void buzzTest() {
+        assertThat(solution.fizzBuzz(3), equalTo("buzz"));
+    }
+
+    @Test
+    public void fizzBuzzTest() {
+        assertThat(solution.fizzBuzz(3), equalTo("fizz buzz"));
+    }
+
+    @Test
+    public void noFizzBuzzTest() {
+        assertThat(solution.fizzBuzz(29), equalTo("29"));
     }
 }
