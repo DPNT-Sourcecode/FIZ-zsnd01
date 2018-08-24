1a201cd9eb2561ad4ee7d31d200299c893862eab
sbx_user1060 <sbx_user1060@ip-10-129-23-199.eu-west-2.compute.internal>
2018-08-24 12:44:15 +0000

    Fri Aug 24 12:44:15 UTC 2018

--- src/main/java/befaster/solutions/FIZ/FizzBuzzSolution.java
+++ src/main/java/befaster/solutions/FIZ/FizzBuzzSolution.java
@@ -1,31 +1,31 @@
 package befaster.solutions.FIZ;
 
-import befaster.runner.SolutionNotImplementedException;
-
 public class FizzBuzzSolution {
+
+    public static final String FIZZ_BUZZ = "fizz buzz";
 
     public String fizzBuzz(Integer number) {
 
         if((number % 3 ==0) && (number % 5 == 0) &&
-                (number > 10 && String.valueOf(number).chars().distinct().count() == 1)) {
-            return "fizz buzz deluxe";
+                isDeluxe(number)) {
+            return FIZZ_BUZZ + " " + fakeDeluxe(number);
         }
-       else if(number % 3 ==0 && (number > 10 && String.valueOf(number).chars().distinct().count() == 1)) {
-            return "fizz deluxe";
-        } else if(number % 5 == 0 && (number > 10 && String.valueOf(number).chars().distinct().count() == 1)) {
-            return "buzz deluxe";
-        }  else if(number > 10 && String.valueOf(number).chars().distinct().count() == 1) {
-            return "deluxe";
+        else if(number % 3 ==0 && isDeluxe(number)) {
+            return "fizz "+ fakeDeluxe(number);
+        } else if(number % 5 == 0 && isDeluxe(number)) {
+            return "buzz "+ fakeDeluxe(number);
+        }  else if(isDeluxe(number)) {
+            return fakeDeluxe(number);
         }
         else if((number % 3 ==0) && (number % 5 == 0) ||
                 (String.valueOf(number).contains("3") && String.valueOf(number).contains("5"))) {
-            return "fizz buzz";
+            return FIZZ_BUZZ;
         } else if((number % 3 ==0) &&
                 String.valueOf(number).contains("5")) {
-            return "fizz buzz";
+            return FIZZ_BUZZ;
         }else  if(number % 5 == 0 &&
                 (String.valueOf(number).contains("3"))) {
-            return "fizz buzz";
+            return FIZZ_BUZZ;
         }
         else if(number % 3 ==0 || String.valueOf(number).contains("3")) {
             return "fizz";
@@ -35,6 +35,14 @@ public class FizzBuzzSolution {
             return String.valueOf(number);
         }
 
+    }
+
+    private boolean isDeluxe(Integer number) {
+        return number > 10 && String.valueOf(number).chars().distinct().count() == 1;
+    }
+
+    private String fakeDeluxe(Integer number) {
+        return number%2 ==0 ? "deluxe" : "fake deluxe";
     }
 
 }
