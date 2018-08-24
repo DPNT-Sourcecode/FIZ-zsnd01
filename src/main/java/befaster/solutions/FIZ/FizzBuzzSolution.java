27059fd3ca681195f9c3a4f5b75c4f607b813aac
sbx_user1060 <sbx_user1060@ip-10-129-23-199.eu-west-2.compute.internal>
2018-08-24 12:53:27 +0000

    Fri Aug 24 12:53:27 UTC 2018

--- src/main/java/befaster/solutions/FIZ/FizzBuzzSolution.java
+++ src/main/java/befaster/solutions/FIZ/FizzBuzzSolution.java
@@ -7,15 +7,13 @@ public class FizzBuzzSolution {
     public String fizzBuzz(Integer number) {
 
         if((number % 3 ==0) && (number % 5 == 0) &&
-                isDeluxe(number)) {
+                (String.valueOf(number).contains("3") || String.valueOf(number).contains("5"))) {
             return FIZZ_BUZZ + " " + fakeDeluxe(number);
         }
-        else if(number % 3 ==0 && isDeluxe(number)) {
+        else if(number % 3 ==0 && (String.valueOf(number).contains("3"))) {
             return "fizz "+ fakeDeluxe(number);
-        } else if(number % 5 == 0 && isDeluxe(number)) {
+        } else if(number % 5 == 0 && (String.valueOf(number).contains("5"))) {
             return "buzz "+ fakeDeluxe(number);
-        }  else if(isDeluxe(number)) {
-            return fakeDeluxe(number);
         }
         else if((number % 3 ==0) && (number % 5 == 0) ||
                 (String.valueOf(number).contains("3") && String.valueOf(number).contains("5"))) {
@@ -35,10 +33,6 @@ public class FizzBuzzSolution {
             return String.valueOf(number);
         }
 
-    }
-
-    private boolean isDeluxe(Integer number) {
-        return number > 10 && String.valueOf(number).chars().distinct().count() == 1;
     }
 
     private String fakeDeluxe(Integer number) {
